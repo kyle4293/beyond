c951555c62a3807d14f719906421749aeda382c7
kyle <chang4293@gmail.com>
2022-12-14 20:47:12 +0900

    1214_4

--- pybo/views.py
+++ pybo/views.py
@@ -6,3 +6,8 @@ def index(request):
     post_list = Post.objects.order_by('-create_date')
     context = {'post_list': post_list}
     return render(request, 'pybo/post_list.html', context)
+
+def detail(request, post_id):
+    post = Post.objects.get(id=post_id)
+    context = {'post': post}
+    return render(request, 'pybo/post_detail.html', context)    
