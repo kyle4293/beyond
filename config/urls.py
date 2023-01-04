b481c7cbe8d67c49d905cc00b6b615671499598c
kyle <chang4293@gmail.com>
2023-01-04 20:35:44 +0900

    0104_4

--- config/urls.py
+++ config/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('common/', include('common.urls')),
     path('pybo/', include('pybo.urls')),
     path('admin/', admin.site.urls),
 ]
